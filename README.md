32f020369f6a8f05968dc30d843262d28a558bf9
VArtz <farrelnikoson@gmail.com>
2024-06-14 18:31:12 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,60 @@
+# NEOCURL
+
+## Simple, effective and fast HTTP Client based on curl for neovim
+
+The neocurl plugin are designed for ease to use and getting work done fast so you can focus on your development!
+
+### Installation
+
+### Depedencies
+
+- curl
+- jq
+
+### Using neocurl example
+
+- Create .http file
+```
+### Register user
+POST http://localhost:3000/api/users
+Content-Type: application/json
+Accept: application/json
+
+{
+  "username": "VArtz",
+  "password" : "secret123",
+  "name" : "Farrel Nikoson"
+}
+
+### Login User
+POST http://localhost:3000/api/users/login
+Content-Type: application/json
+Accept: application/json
+
+{
+  "username": "VArtz",
+  "password" : "secret123"
+}
+
+### Get User
+GET http://localhost:3000/api/users/current
+Accept: application/json
+X-API-TOKEN: 25edd380-731c-4d9f-ac9b-b49f01e205bd
+```
+
+- Select block that you want to run with place your cursor in block area (from ### to \n###)
+```
+### Get User // place your cursor here
+GET http://localhost:3000/api/users/current // or here
+Accept: application/json // or here
+X-API-TOKEN: 25edd380-731c-4d9f-ac9b-b49f01e205bd// or here
+// or here
+### Another... // This will run another block
+```
+
+- Run :Http, :Httpv or :Httpd in nvim command
+```:Http``` -- formatted
+```:Httpv``` -- verbose version (using curl -v)
+```:Httpd``` -- debug mode, curl statement will appear on statusline
+
+- Seperate window will appear and provide response from curl http request
---
+++ main.lua
@@ -0,0 +1,79 @@
+local function parse_and_execute_http_request(lines, verbose, debug)
+    local method, url, headers, body
+    headers = {}
+    body = ""
+
+    -- Parse lines to extract HTTP request details
+    local in_body = false
+    for _, line in ipairs(lines) do
+        if not in_body then
+            if line:match("^%s*(%u+)%s+(http.+)$") then
+                method, url = line:match("^%s*(%u+)%s+(http.+)$")
+            elseif line:match("^%s*([%w-]+)%s*:%s*(.+)$") then
+                local header, value = line:match("^%s*([%w-]+)%s*:%s*(.+)$")
+                table.insert(headers, string.format('-H "%s: %s"', header, value))
+            elseif line:match("^%s*{") then
+                in_body = true
+                body = body .. line
+            end
+        else
+            if line:match("^%s*}$") then
+                in_body = false
+            end
+            body = body .. line
+        end
+    end
+
+    -- Construct curl command
+    local curl_command = 'curl -s -X ' .. method .. ' "' .. url .. '"'
+    if headers then
+        curl_command = curl_command .. " " .. table.concat(headers, " ")
+    end
+    if body and body ~= "" then
+        curl_command = curl_command .. ' -d "' .. body:gsub('"', '\\"') .. '"'
+    end
+    if verbose then
+        curl_command = curl_command .. " -v"
+    end
+    if debug then
+        print(curl_command)
+    end
+
+    -- Execute curl command in a new split
+    vim.cmd("vsplit")
+    vim.cmd("term " .. curl_command .. " | jq")
+end
+
+-- Function to execute HTTP requests from selected visual block
+function execute_http_request_from_visual(verbose, debug)
+    local cursor_pos = vim.api.nvim_win_get_cursor(0)
+    local total_lines = vim.api.nvim_buf_line_count(0)
+    local current_line = cursor_pos[1]
+
+    -- Find the start of the current block
+    local start_line = current_line
+    while start_line > 1 do
+        local line = vim.api.nvim_buf_get_lines(0, start_line - 2, start_line - 1, false)[1]
+        if line:match("^###") then
+            break
+        end
+        start_line = start_line - 1
+    end
+
+    -- Find the end of the current block
+    local end_line = current_line
+    while end_line <= total_lines do
+        local line = vim.api.nvim_buf_get_lines(0, end_line - 1, end_line, false)[1]
+        if line:match("^###") and end_line ~= current_line then
+            break
+        end
+        end_line = end_line + 1
+    end
+
+    -- Get the lines of the current block
+    parse_and_execute_http_request(vim.api.nvim_buf_get_lines(0, start_line - 1, end_line - 1, false), verbose, debug)
+end
+
+vim.cmd("command! Http lua execute_http_request_from_visual()")
+vim.cmd("command! Httpv lua execute_http_request_from_visual(true)")
+vim.cmd("command! Httpd lua execute_http_request_from_visual(false, true)")
